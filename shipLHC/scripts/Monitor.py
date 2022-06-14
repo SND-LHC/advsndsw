cb489fed40aeb1ae517f52c6556c15377f7b10be
ThomasRuf <Thomas.Ruf@cern.ch>
2022-06-14 16:25:47 +0200

    delete tracks at start of next event to avoid memory leak

--- shipLHC/scripts/Monitor.py
+++ shipLHC/scripts/Monitor.py
@@ -527,6 +527,7 @@ class TrackSelector():
 
    def Execute(self):
       for n in range(self.options.nStart,self.options.nStart+self.options.nEvents):
+          for aTrack in self.OT.Reco_MuonTracks: aTrack.Delete()
           self.eventTree.GetEvent(n)
           self.ExecuteEvent(self.eventTree)
           if self.OT.Reco_MuonTracks.GetEntries()>0:
