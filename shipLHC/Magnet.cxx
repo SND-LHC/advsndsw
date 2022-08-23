d3ad52aaaf015e18fc99dfa6be6e09b4a102e7cf
dcentanni <daniele.centanni@outlook.it>
2022-08-23 14:18:57 +0200

    Implement AdvSND magnet

---
+++ shipLHC/Magnet.cxx
@@ -0,0 +1,304 @@
+//
+//  Magnet.cxx
+//
+//  by A. Escalante, D. Centanni
+//  Aug 2022
+//
+
+#include "Magnet.h"
+#include "MagnetPoint.h"
+
+#include "TGeoManager.h"
+#include "FairRun.h"                    // for FairRun
+#include "FairRuntimeDb.h"              // for FairRuntimeDb
+#include <iosfwd>                    // for ostream
+#include "TList.h"                      // for TListIter, TList (ptr only)
+#include "TObjArray.h"                  // for TObjArray
+#include "TString.h"                    // for TString
+
+#include "TGeoBBox.h"
+#include "TGeoTrd1.h"
+#include "TGeoSphere.h"
+#include "TGeoCompositeShape.h"
+#include "TGeoTube.h"
+#include "TGeoMaterial.h"
+#include "TGeoMedium.h"
+#include "TGeoTrd1.h"
+#include "TGeoArb8.h"
+#include "TParticle.h"
+
+#include "TClonesArray.h"
+#include "TVirtualMC.h"
+
+#include "FairVolume.h"
+#include "FairGeoVolume.h"
+#include "FairGeoNode.h"
+#include "FairRootManager.h"
+#include "FairGeoLoader.h"
+#include "FairGeoInterface.h"
+#include "FairGeoTransform.h"
+#include "FairGeoMedia.h"
+#include "FairGeoMedium.h"
+#include "FairGeoBuilder.h"
+#include "FairRun.h"
+#include "FairRuntimeDb.h"
+
+#include "ShipDetectorList.h"
+#include "ShipUnit.h"
+#include "ShipStack.h"
+
+#include "TGeoTrd2.h" 
+#include "TGeoCompositeShape.h"
+
+#include "TGeoUniformMagField.h"
+#include "TVector3.h"
+#include <stddef.h>                     // for NULL
+#include <iostream>                     // for operator<<, basic_ostream,etc
+#include <string.h>
+
+using std::cout;
+using std::endl;
+
+using namespace ShipUnit;
+
+Magnet::Magnet()
+: FairDetector("Magnet", "",kTRUE),
+  fTrackID(-1),
+  fVolumeID(-1),
+  fPos(),
+  fMom(),
+  fTime(-1.),
+  fLength(-1.),
+  fELoss(-1),
+  fMagnetPointCollection(new TClonesArray("MagnetPoint"))
+{
+}
+
+Magnet::Magnet(const char* name, Bool_t Active,const char* Title)
+: FairDetector(name, true, kTRUE),
+  fTrackID(-1),
+  fVolumeID(-1),
+  fPos(),
+  fMom(),
+  fTime(-1.),
+  fLength(-1.),
+  fELoss(-1),
+  fMagnetPointCollection(new TClonesArray("MagnetPoint"))
+{
+}
+
+Magnet::~Magnet()
+{
+    if (fMagnetPointCollection) {
+        fMagnetPointCollection->Delete();
+        delete fMagnetPointCollection;
+    }
+}
+
+void Magnet::Initialize()
+{
+  FairDetector::Initialize();
+}
+
+
+// -----   Private method InitMedium
+Int_t Magnet::InitMedium(const char* name)
+{
+  static FairGeoLoader *geoLoad=FairGeoLoader::Instance();
+  static FairGeoInterface *geoFace=geoLoad->getGeoInterface();
+  static FairGeoMedia *media=geoFace->getMedia();
+  static FairGeoBuilder *geoBuild=geoLoad->getGeoBuilder();
+    
+  FairGeoMedium *ShipMedium=media->getMedium(name);
+    
+  if (!ShipMedium)
+    {
+      Fatal("InitMedium","Material %s not defined in media file.", name);
+      return -1111;
+    }
+  TGeoMedium* medium=gGeoManager->GetMedium(name);
+  if (medium!=NULL)
+    return ShipMedium->getMediumIndex();
+  return geoBuild->createMedium(ShipMedium);
+}
+
+void Magnet::ConstructGeometry()
+{
+    // Geometry implementation from D. Centanni
+
+    TGeoVolume *top=gGeoManager->GetTopVolume();
+    TGeoVolume *detector = gGeoManager->FindVolumeFast("Detector");
+    if(!detector)  LOG(ERROR) << "no Detector volume found " ;
+
+    // Materials
+
+    InitMedium("air");
+    TGeoMedium *air =gGeoManager->GetMedium("air");  
+
+    InitMedium("iron");
+    TGeoMedium *Fe =gGeoManager->GetMedium("iron");
+
+    InitMedium("CoilCopper");
+    TGeoMedium *Cu  = gGeoManager->GetMedium("CoilCopper");
+
+    // MAGNET STRUCTURE
+    Double_t fInMagX = conf_floats["Magnet/InMagX"];
+    Double_t fInMagY = conf_floats["Magnet/InMagY"];
+    Double_t fIronYokeX = conf_floats["Magnet/IronYokeX"];
+    Double_t fIronYokeY = conf_floats["Magnet/IronYokeY"];
+    Double_t fCoilX = conf_floats["Magnet/CoilX"];
+    Double_t fCoilY = conf_floats["Magnet/CoilY"];
+    Double_t fOutMagX = conf_floats["Magnet/OutMagX"];
+    Double_t fOutMagY = conf_floats["Magnet/OutMagY"];
+    Double_t fMagZ = conf_floats["Magnet/MagZ"];
+
+    // MAGNETIC FIELD
+    Double_t fField = conf_floats["Magnet/Field"];
+
+    // TRACKING STATIONS STRUCTURE
+    Double_t fTrackerZ = conf_floats["Magnet/TrackerZ"];
+    Double_t fTSpacingZ = conf_floats["Magnet/TSpacingZ"];
+    Double_t fLevArm = conf_floats["Magnet/LevArm"];
+
+	// Shifts
+  	Double_t fShiftX     = conf_floats["Magnet/ShiftX"];
+  	Double_t fShiftY     = conf_floats["Magnet/ShiftY"];
+  	Double_t fShiftZ     = conf_floats["Magnet/ShiftZ"];
+
+    // Magnetic Field
+    TGeoUniformMagField *magField = new TGeoUniformMagField(-fField,0, 0);
+
+    TGeoVolumeAssembly *MagnetVol = new TGeoVolumeAssembly("Magnet");
+
+
+    // Shapes creation
+    TGeoBBox *CoilContainer = new TGeoBBox("CoilContainer", fOutMagX/2., fOutMagY/2., fMagZ/2.);
+    TGeoBBox *MagRegion = new TGeoBBox("MagRegion", fInMagX/2., fInMagY/2., fMagZ/2.+0.5);
+    TGeoBBox *Coil = new TGeoBBox("Coil", fCoilX/2., fCoilY/2., fMagZ/2.+0.5);
+
+    // Translations
+    TGeoTranslation *CoilUpPos = new TGeoTranslation("CoilUpPos", 0, (fInMagY+fCoilY)/2.-0.001, 0);
+    TGeoTranslation *CoilDownPos = new TGeoTranslation("CoilDownPos", 0, -(fInMagY+fCoilY)/2.+0.001, 0);
+    CoilUpPos->RegisterYourself();
+    CoilDownPos->RegisterYourself();
+
+    // Yoke shape
+    TGeoCompositeShape *FeYoke = new TGeoCompositeShape("FeYoke", "CoilContainer-MagRegion-(Coil:CoilUpPos)-(Coil:CoilDownPos)");
+
+    // Volumes
+    TGeoVolume *volFeYoke = new TGeoVolume("volFeYoke", FeYoke, Fe);
+    volFeYoke->SetLineColor(kGray);
+    TGeoVolume *volCoil = new TGeoVolume("volCoil", Coil, Cu);
+    volCoil->SetLineColor(kOrange+1);
+    TGeoVolume *volMagRegion = new TGeoVolume("volMagRegion", MagRegion, air);
+    volMagRegion->SetField(magField);
+
+    // Positioning
+    MagnetVol->AddNode(volFeYoke, 0);
+    MagnetVol->AddNode(volCoil, 0, new TGeoTranslation(0, (fInMagY+fCoilY)/2., 0));
+    MagnetVol->AddNode(volCoil, 1, new TGeoTranslation(0, -(fInMagY+fCoilY)/2., 0));
+    MagnetVol->AddNode(volMagRegion, 0, 0);
+
+
+    TGeoBBox *TrackPlane = new TGeoBBox("TrackPlane", fInMagX/2., fInMagY/2., fTrackerZ/2.);
+
+    // TO BE PROPERLY ADDED
+    TGeoVolume *volTrackPlane = new TGeoVolume("volTrackPlane", TrackPlane, 0);
+    volTrackPlane->SetLineColorAlpha(kBlue, 0.4);
+    AddSensitiveVolume(volTrackPlane);
+
+    MagnetVol->AddNode(volTrackPlane, 1, new TGeoTranslation(0, 0, -fMagZ/2.-fTSpacingZ-fTrackerZ/2.));
+    MagnetVol->AddNode(volTrackPlane, 2, new TGeoTranslation(0, 0, +fMagZ/2.+fTSpacingZ+fTrackerZ/2.));
+    MagnetVol->AddNode(volTrackPlane, 0, new TGeoTranslation(0, 0, -fMagZ/2.-fTSpacingZ-fTrackerZ-fLevArm-fTrackerZ/2.));
+    MagnetVol->AddNode(volTrackPlane, 3, new TGeoTranslation(0, 0, +fMagZ/2.+fTSpacingZ+fTrackerZ+fLevArm+fTrackerZ/2.));
+
+    detector->AddNode(MagnetVol, 0, new TGeoTranslation(fShiftX,fShiftY,fShiftZ)); // see Alberto implementation
+}
+
+Bool_t  Magnet::ProcessHits(FairVolume* vol)
+{
+  /** This method is called from the MC stepping */
+  //Set parameters at entrance of volume. Reset ELoss.
+  if ( gMC->IsTrackEntering() ) {
+    fELoss  = 0.;
+    fTime   = gMC->TrackTime() * 1.0e09;
+    fLength = gMC->TrackLength();
+    gMC->TrackPosition(fPos);
+    gMC->TrackMomentum(fMom);
+  }
+  // Sum energy loss for all steps in the active volume
+  fELoss += gMC->Edep();
+
+  // Create vetoPoint at exit of active volume
+  if (  gMC->IsTrackExiting()    ||
+        gMC->IsTrackStop()       ||
+        gMC->IsTrackDisappeared()   ) {
+        if (fELoss == 0. ) {return kFALSE; }
+
+    fTrackID  = gMC->GetStack()->GetCurrentTrackNumber();
+
+    TParticle* p=gMC->GetStack()->GetCurrentTrack();
+    Int_t pdgCode = p->GetPdgCode();
+    TLorentzVector Pos;
+    gMC->TrackPosition(Pos);
+    TLorentzVector Mom;
+    gMC->TrackMomentum(Mom);
+    Int_t detID = 0;
+    gMC->CurrentVolID(detID);
+    const char *name;
+    name = gMC->CurrentVolName();
+    // Check which volume is actually hit and what detID is given
+    LOG(DEBUG)<< "MagnetPoint DetID " << detID << " Hit volume name " << name;
+    fVolumeID = detID;
+    Double_t xmean = (fPos.X()+Pos.X())/2. ;
+	  Double_t ymean = (fPos.Y()+Pos.Y())/2. ;
+	  Double_t zmean = (fPos.Z()+Pos.Z())/2. ;
+    AddHit(fTrackID, detID,  TVector3(xmean, ymean,  zmean),
+			TVector3(fMom.Px(), fMom.Py(), fMom.Pz()), fTime, fLength,
+			fELoss, pdgCode);
+    }
+
+    return kTRUE;
+}
+
+void Magnet::EndOfEvent()
+{
+    fMagnetPointCollection->Clear();
+}
+
+void Magnet::Register()
+{
+    /** This will create a branch in the output tree called
+     MagnetPoint, setting the last parameter to kFALSE means:
+     this collection will not be written to the file, it will exist
+     only during the simulation.
+     */
+
+    FairRootManager::Instance()->Register("MagnetPoint", "Magnet",
+                                            fMagnetPointCollection, kTRUE);
+}
+
+TClonesArray* Magnet::GetCollection(Int_t iColl) const
+{
+    if (iColl == 0) { return fMagnetPointCollection; }
+    else { return NULL; }
+}
+
+void Magnet::Reset()
+{
+    fMagnetPointCollection->Clear();
+}
+
+MagnetPoint* Magnet::AddHit(Int_t trackID, Int_t detID,
+                           TVector3 pos, TVector3 mom,
+                           Double_t time, Double_t length,
+                           Double_t eLoss, Int_t pdgCode)
+{
+    TClonesArray& clref = *fMagnetPointCollection;
+    Int_t size = clref.GetEntriesFast();
+    return new(clref[size]) MagnetPoint(trackID, detID, pos, mom,
+                                       time, length, eLoss, pdgCode);
+}
+
+
+ClassImp(Magnet)
---
+++ shipLHC/Magnet.h
@@ -0,0 +1,112 @@
+//
+//  Magnet.h
+//
+//  by A. Escalante, D. Centanni
+//  Aug 2022
+//
+#ifndef Magnet_H
+#define Magnet_H
+
+
+#include "FairModule.h"                 // for FairModule
+#include "FairDetector.h"
+
+#include "Rtypes.h"                     // for ShipMuonShield::Class, Bool_t, etc
+
+#include <string>                       // for string
+
+#include "TVector3.h"
+#include "TLorentzVector.h"
+
+class MagnetPoint;
+class FairVolume;
+class TClonesArray;
+
+class Magnet : public FairDetector
+{
+public:
+    Magnet(const char* name, Bool_t Active, const char* Title="Magnet");
+    Magnet();
+    virtual ~Magnet();
+
+    /** Create the detector geometry **/
+    void ConstructGeometry();
+
+    void SetConfPar(TString name, Float_t value){conf_floats[name]=value;}
+    void SetConfPar(TString name, Int_t value){conf_ints[name]=value;}
+    void SetConfPar(TString name, TString value){conf_strings[name]=value;}
+    Float_t  GetConfParF(TString name){return conf_floats[name];}
+    Int_t       GetConfParI(TString name){return conf_ints[name];}
+    TString  GetConfParS(TString name){return conf_strings[name];}
+
+    /** Initialization of the detector is done here    */
+    virtual void Initialize();
+
+    /**       this method is called for each step during simulation
+    *       (see FairMCApplication::Stepping())
+    */
+    virtual Bool_t ProcessHits( FairVolume* v=0);
+
+    /**       Registers the produced collections in FAIRRootManager.    */
+    virtual void   Register();
+
+    /** Gets the produced collections */
+    virtual TClonesArray* GetCollection(Int_t iColl) const ;
+
+    /**      has to be called after each event to reset the containers      */
+    virtual void   Reset();
+
+    /**      This method is an example of how to add your own point
+    *       of type muonPoint to the clones array
+    */
+    MagnetPoint* AddHit(Int_t trackID, Int_t detID,
+                    TVector3 pos, TVector3 mom,
+                    Double_t time, Double_t length,
+                    Double_t eLoss, Int_t pdgCode);
+    
+    /** The following methods can be implemented if you need to make
+     *  any optional action in your detector during the transport.
+     */
+    
+    virtual void   CopyClones( TClonesArray* cl1,  TClonesArray* cl2 ,
+                              Int_t offset) {;}
+    virtual void   SetSpecialPhysicsCuts() {;}
+    virtual void   EndOfEvent();
+    virtual void   FinishPrimary() {;}
+    virtual void   FinishRun() {;}
+    virtual void   BeginPrimary() {;}
+    virtual void   PostTrack() {;}
+    virtual void   PreTrack() {;}
+    virtual void   BeginEvent() {;}
+
+    Magnet(const Magnet&);
+    Magnet& operator=(const Magnet&);
+
+private:
+    
+    /** Track information to be stored until the track leaves the
+     active volume.
+     */
+    Int_t          fTrackID;           //!  track index
+    Int_t          fVolumeID;          //!  volume id
+    TLorentzVector fPos;               //!  position at entrance
+    TLorentzVector fMom;               //!  momentum at entrance
+    Double32_t     fTime;              //!  time
+    Double32_t     fLength;            //!  length
+    Double32_t     fELoss;             //!  energy loss
+    /** container for data points */
+    TClonesArray*  fMagnetPointCollection;
+    /** configuration parameters **/
+    std::map<TString,Float_t> conf_floats;
+    std::map<TString,Int_t> conf_ints;
+    std::map<TString,TString> conf_strings;
+
+    ClassDef(Magnet,1) // why 2 ?
+
+protected:
+    
+    Int_t InitMedium(const char* name);
+    
+};
+
+#endif
