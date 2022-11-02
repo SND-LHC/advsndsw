9f1e89608bd2562791c3b819692c83fd5cef0afe
siilieva <simona.ilieva.ilieva@cern.ch>
2022-11-02 14:36:56 +0100

    Add GetStation() method for MuFilter hits. Function by the same name already exists for SciFi hits. Station numbering will start from 1 for each MuFilter sub-system.

--- shipLHC/MuFilterHit.h
+++ shipLHC/MuFilterHit.h
@@ -37,6 +37,7 @@ class MuFilterHit : public SndlhcHit
     bool isMasked(Int_t i) const {return fMasked[i];}
     void SetMasked(Int_t i) {fMasked[i]=kTRUE;}
     int GetSystem(){return floor(fDetectorID/10000);}
+    int GetStation(){return floor(fDetectorID/1000)+1;}
     bool isVertical();
     bool isShort(Int_t);
   private:
