1a8bc20bb8a15300a3b85c763c568093ac8dbfbd
Oliver Lantwin <oliver.lantwin@cern.ch>
2023-01-25 11:22:28 +0100

    Implement more realistic target using TOB

--- shipLHC/AdvTarget.cxx
+++ shipLHC/AdvTarget.cxx
@@ -9,12 +9,12 @@
 #include "AdvTargetPoint.h"
 
 #include "TGeoManager.h"
-#include "FairRun.h"                    // for FairRun
-#include "FairRuntimeDb.h"              // for FairRuntimeDb
-#include <iosfwd>                    // for ostream
-#include "TList.h"                      // for TListIter, TList (ptr only)
-#include "TObjArray.h"                  // for TObjArray
-#include "TString.h"                    // for TString
+#include "FairRun.h"       // for FairRun
+#include "FairRuntimeDb.h" // for FairRuntimeDb
+#include <iosfwd>          // for ostream
+#include "TList.h"         // for TListIter, TList (ptr only)
+#include "TObjArray.h"     // for TObjArray
+#include "TString.h"       // for TString
 
 #include "TGeoBBox.h"
 #include "TGeoTrd1.h"
@@ -47,222 +47,266 @@
 #include "ShipUnit.h"
 #include "ShipStack.h"
 
-#include "TGeoTrd2.h" 
+#include "TGeoTrd2.h"
 #include "TGeoCompositeShape.h"
 
 #include "TGeoUniformMagField.h"
 #include "TGeoGlobalMagField.h"
 #include "TVector3.h"
-#include <stddef.h>                     // for NULL
-#include <iostream>                     // for operator<<, basic_ostream,etc
+#include <ROOT/TSeq.hxx>
+#include <stddef.h> // for NULL
+#include <iostream> // for operator<<, basic_ostream,etc
 #include <string.h>
 
-using std::cout;
-using std::endl;
-
+using ROOT::TSeq;
 using namespace ShipUnit;
 
 AdvTarget::AdvTarget()
-: FairDetector("AdvTarget", "",kTRUE),
-  fTrackID(-1),
-  fVolumeID(-1),
-  fPos(),
-  fMom(),
-  fTime(-1.),
-  fLength(-1.),
-  fELoss(-1),
-  fAdvTargetPointCollection(new TClonesArray("AdvTargetPoint"))
+   : FairDetector("AdvTarget", "", kTRUE), fTrackID(-1), fVolumeID(-1), fPos(), fMom(), fTime(-1.), fLength(-1.),
+     fELoss(-1), fAdvTargetPointCollection(new TClonesArray("AdvTargetPoint"))
 {
 }
 
-AdvTarget::AdvTarget(const char* name, Bool_t Active,const char* Title)
-: FairDetector(name, true, kAdvSNDTarget),
-  fTrackID(-1),
-  fVolumeID(-1),
-  fPos(),
-  fMom(),
-  fTime(-1.),
-  fLength(-1.),
-  fELoss(-1),
-  fAdvTargetPointCollection(new TClonesArray("AdvTargetPoint"))
+AdvTarget::AdvTarget(const char *name, Bool_t Active, const char *Title)
+   : FairDetector(name, true, kAdvSNDTarget), fTrackID(-1), fVolumeID(-1), fPos(), fMom(), fTime(-1.), fLength(-1.),
+     fELoss(-1), fAdvTargetPointCollection(new TClonesArray("AdvTargetPoint"))
 {
 }
 
 AdvTarget::~AdvTarget()
 {
-    if (fAdvTargetPointCollection) {
-        fAdvTargetPointCollection->Delete();
-        delete fAdvTargetPointCollection;
-    }
+   if (fAdvTargetPointCollection) {
+      fAdvTargetPointCollection->Delete();
+      delete fAdvTargetPointCollection;
+   }
 }
 
 void AdvTarget::Initialize()
 {
-  FairDetector::Initialize();
+   FairDetector::Initialize();
 }
 
-
 // -----   Private method InitMedium
-Int_t AdvTarget::InitMedium(const char* name)
+Int_t AdvTarget::InitMedium(const char *name)
 {
-  static FairGeoLoader *geoLoad=FairGeoLoader::Instance();
-  static FairGeoInterface *geoFace=geoLoad->getGeoInterface();
-  static FairGeoMedia *media=geoFace->getMedia();
-  static FairGeoBuilder *geoBuild=geoLoad->getGeoBuilder();
-    
-  FairGeoMedium *ShipMedium=media->getMedium(name);
-    
-  if (!ShipMedium)
-    {
-      Fatal("InitMedium","Material %s not defined in media file.", name);
+   static FairGeoLoader *geoLoad = FairGeoLoader::Instance();
+   static FairGeoInterface *geoFace = geoLoad->getGeoInterface();
+   static FairGeoMedia *media = geoFace->getMedia();
+   static FairGeoBuilder *geoBuild = geoLoad->getGeoBuilder();
+
+   FairGeoMedium *ShipMedium = media->getMedium(name);
+
+   if (!ShipMedium) {
+      Fatal("InitMedium", "Material %s not defined in media file.", name);
       return -1111;
-    }
-  TGeoMedium* medium=gGeoManager->GetMedium(name);
-  if (medium!=NULL)
-    return ShipMedium->getMediumIndex();
-  return geoBuild->createMedium(ShipMedium);
+   }
+   TGeoMedium *medium = gGeoManager->GetMedium(name);
+   if (medium != NULL)
+      return ShipMedium->getMediumIndex();
+   return geoBuild->createMedium(ShipMedium);
 }
 
 void AdvTarget::ConstructGeometry()
 {
-    // Geometry implementation from D. Centanni
-    TGeoVolume *top=gGeoManager->GetTopVolume();
-    TGeoVolume *detector = gGeoManager->FindVolumeFast("Detector");
-    if(!detector)  LOG(ERROR) << "no Detector volume found " ;
+   // Geometry implementation from D. Centanni
+   TGeoVolume *top = gGeoManager->GetTopVolume();
+   TGeoVolume *detector = gGeoManager->FindVolumeFast("Detector");
+   if (!detector)
+      LOG(ERROR) << "no Detector volume found ";
 
-    // Materials
+   // Materials
 
-    InitMedium("tungstensifon");
-	  TGeoMedium *tungsten = gGeoManager->GetMedium("tungstensifon");
+   InitMedium("tungstensifon");
+   TGeoMedium *tungsten = gGeoManager->GetMedium("tungstensifon");
 
-    InitMedium("Polystyrene");
-    TGeoMedium *Polystyrene = gGeoManager->GetMedium("Polystyrene");
+   InitMedium("Polystyrene");
+   TGeoMedium *Polystyrene = gGeoManager->GetMedium("Polystyrene");
+   InitMedium("silicon");
+   TGeoMedium *Silicon = gGeoManager->GetMedium("silicon");
 
-    Double_t fTargetWallX = conf_floats["AdvTarget/TargetWallX"];
-    Double_t fTargetWallY = conf_floats["AdvTarget/TargetWallY"];
-    Double_t fTargetWallZ = conf_floats["AdvTarget/TargetWallZ"];
-    Double_t fTTX = conf_floats["AdvTarget/TTX"];
-    Double_t fTTY = conf_floats["AdvTarget/TTY"];
-    Double_t fTTZ = conf_floats["AdvTarget/TTZ"];
-    Int_t fnTT = conf_ints["AdvTarget/nTT"];
+   Double_t fTargetWallX = conf_floats["AdvTarget/TargetWallX"];
+   Double_t fTargetWallY = conf_floats["AdvTarget/TargetWallY"];
+   Double_t fTargetWallZ = conf_floats["AdvTarget/TargetWallZ"];
+   Double_t fTTX = conf_floats["AdvTarget/TTX"];
+   Double_t fTTY = conf_floats["AdvTarget/TTY"];
+   Double_t fTTZ = conf_floats["AdvTarget/TTZ"];
+   Int_t stations = conf_ints["AdvTarget/nTT"]; // Number of TT stations
 
-    TGeoBBox *TargetWall = new TGeoBBox("TargetWall", fTargetWallX/2., fTargetWallY/2., fTargetWallZ/2.);
-    TGeoBBox *TTracker = new TGeoBBox("TTracker", fTTX/2., fTTY/2., fTTZ/2.);
+   TGeoBBox *TargetWall = new TGeoBBox("TargetWall", fTargetWallX / 2., fTargetWallY / 2., fTargetWallZ / 2.);
+   TGeoVolume *volTargetWall = new TGeoVolume("volTargetWall", TargetWall, tungsten);
+   volTargetWall->SetLineColor(kRed);
 
-    TGeoVolume *volTargetWall = new TGeoVolume("volTargetWall", TargetWall, tungsten);
-    TGeoVolume *volTTracker = new TGeoVolume("volTTracker", TTracker, Polystyrene);
+   // Silicon tracker module
+   //
+   // See https://indico.cern.ch/event/1201858/#81-detector-simulation for technical diagrams and renders
+   //
+   // Passive part
+   double module_length = 23.95 * cm;
+   double module_width = 12.0 * cm;
+   TGeoBBox *Support = new TGeoBBox("Support", module_length / 2, module_width / 2, 3.0 * mm / 2);
+   TGeoVolume *SupportVolume = new TGeoVolume("SupportVolume", Support, Polystyrene);
+   SupportVolume->SetLineColor(kGray);
+   // Active part
+   TGeoBBox *Sensor = new TGeoBBox("Sensor", 91.5 * mm / 2, 93.7 * mm / 2, 0.5 * mm / 2);
+   TGeoVolume *SensorVolume = new TGeoVolume("SensorVolume", Sensor, Silicon);
+   SensorVolume->SetLineColor(kGreen);
+   AddSensitiveVolume(SensorVolume);
 
-    volTargetWall->SetLineColor(kYellow-4);
-    volTTracker->SetLineColor(kGray);
+   double sensor_gap = 3.1 * mm;
 
-    //Definition of the target box containing tungsten walls + target trackers (TT) 
-    TGeoVolumeAssembly *volAdvTarget  = new TGeoVolumeAssembly("volAdvTarget");
+   const int rows = 4;
+   const int columns = 2;
+   double module_row_gap = 0.5 * mm;
+   double module_column_gap = 13.9 * mm;
 
-    detector->AddNode(volAdvTarget,1,new TGeoTranslation(-2.4244059999999976,0,0));
+   // Definition of the target box containing tungsten walls + silicon tracker
+   TGeoVolumeAssembly *volAdvTarget = new TGeoVolumeAssembly("volAdvTarget");
 
-    // Positioning calculations, to be deleted once the whole AdvSND apparatus
-    // will be positioned correctly
-    TVector3 EmWall0_survey(5.35+42.2/2., 17.2+42.2/2., 288.92+10/2.); // cm
-    Double_t TargetDiff = 100. - 63.941980;
 
-    AddSensitiveVolume(volTTracker);
-    
-    // adding walls & trackers
-    LOG(INFO) << " nTT: " << fnTT;
-    for(int i=0; i<fnTT; i++)
-    {
-        volAdvTarget->AddNode(volTargetWall, i, new TGeoTranslation(-EmWall0_survey.X()+(fTargetWallX-42.2)/2., EmWall0_survey.Y(), -TargetDiff+EmWall0_survey.Z()+i*(fTargetWallZ+fTTZ)+fTargetWallZ/2.));
-        volAdvTarget->AddNode(volTTracker, i, new TGeoTranslation(-EmWall0_survey.X()+(fTargetWallX-42.2)/2., EmWall0_survey.Y(), -TargetDiff+EmWall0_survey.Z()+i*(fTargetWallZ+fTTZ)+fTargetWallZ+fTTZ/2.));
-    }
-    LOG(INFO) <<"  Target X: "<< -EmWall0_survey.X()+(fTargetWallX-42.2)/2.<<"  Y: "<< EmWall0_survey.Y()<< "  Z: "<< EmWall0_survey.Z();
+   // Positioning calculations, TODO delete once the whole AdvSND apparatus is positioned correctly
+   TVector3 EmWall0_survey(5.35 * cm + 42.2 / 2. * cm, 17.2 * cm + 42.2 / 2. * cm, 288.92 * cm + 10 / 2. * cm); // Survey position of the centre of the first emulsion wall
+   Double_t TargetDiff = 100. * cm - 63.941980 * cm;
+
+   double line_of_sight_offset = -2.4244059999999976 * cm;
+   detector->AddNode(volAdvTarget, 1, new TGeoTranslation(
+                        line_of_sight_offset - EmWall0_survey.X() + (fTargetWallX - 42.2 * cm) / 2.,
+                        EmWall0_survey.Y(),
+                        -TargetDiff + EmWall0_survey.Z()
+                     ));
+
+   LOG(INFO) << " nTT: " << stations;
+   // For correct detector IDs, the geometry has to be built back to front, from the top-level
+   for (auto &&station : TSeq(stations)) {
+      TGeoVolumeAssembly *TrackingStation = new TGeoVolumeAssembly("TrackingStation");
+      // Each tracking station consists of X and Y planes
+      for (auto &&plane : TSeq(2)) {
+         TGeoVolumeAssembly *TrackerPlane = new TGeoVolumeAssembly("TrackerPlane");
+         int i = 0;
+         // Each plane consists of 4 modules
+         for (auto &&row : TSeq(rows)) {
+            for (auto &&column : TSeq(columns)) {
+               // Each module in turn consists of two sensors on a support
+               TGeoVolumeAssembly *SensorModule = new TGeoVolumeAssembly("SensorModule");
+               SensorModule->AddNode(SupportVolume, 1);
+               SensorModule->AddNode(
+                  SensorVolume, 10000 * station + 1000 * plane + 100 * row + 10 * column + 1,
+                  new TGeoTranslation(-module_length / 2 + 46.95 * mm + 91.5 * mm / 2, 0, +3 * mm / 2 + 0.5 * mm / 2));
+               SensorModule->AddNode(SensorVolume, 10000 * station + 1000 * plane + 100 * row + 10 * column + 2,
+                                     new TGeoTranslation(-module_length / 2 + 46.95 * mm + 1.5 * 91.5 * mm + sensor_gap, 0,
+                                                         +3 * mm / 2 + 0.5 * mm / 2));
+               TrackerPlane->AddNode(
+                  SensorModule, ++i,
+                  new TGeoCombiTrans(
+                     // Offset modules as needed by row and column
+                     TGeoTranslation((column % 2 ? 1 : -1) * (module_length / 2 + module_column_gap / 2),
+                                     (row - 1) * (-module_width - module_row_gap) - module_row_gap / 2 + module_width / 2, 0),
+                     // Rotate every module of the second column
+                     TGeoRotation(TString::Format("rot%d", i), 0, 0, column * 180)));
+            }
+         }
+         if(plane == 0){
+            // X-plane
+            TrackingStation->AddNode(TrackerPlane, plane);
+         } else if (plane == 1) {
+            // Y-plane
+            TrackingStation->AddNode(
+               TrackerPlane, plane,
+               new TGeoCombiTrans(TGeoTranslation(0, 0, +3.5 * mm + 0.5 * mm), TGeoRotation("y_rot", 0, 0, 90)));
+         }
+      }
+
+      volAdvTarget->AddNode(
+         volTargetWall, station,
+         new TGeoTranslation(
+            0, 0, -fTargetWallZ / 2 + station * fTargetWallZ + station * 7.5 * mm));
+      volAdvTarget->AddNode(TrackingStation, station,
+                            new TGeoTranslation(0, 0,
+                                                (station + 0.5) * fTargetWallZ +
+                                                (station - 0.5) * 7.5 * mm));
+   }
 }
 
-Bool_t  AdvTarget::ProcessHits(FairVolume* vol)
+Bool_t AdvTarget::ProcessHits(FairVolume *vol)
 {
-  /** This method is called from the MC stepping */
-  //Set parameters at entrance of volume. Reset ELoss.
-  if ( gMC->IsTrackEntering() ) {
-    fELoss  = 0.;
-    fTime   = gMC->TrackTime() * 1.0e09;
-    fLength = gMC->TrackLength();
-    gMC->TrackPosition(fPos);
-    gMC->TrackMomentum(fMom);
-  }
-  // Sum energy loss for all steps in the active volume
-  fELoss += gMC->Edep();
+   /** This method is called from the MC stepping */
+   // Set parameters at entrance of volume. Reset ELoss.
+   if (gMC->IsTrackEntering()) {
+      fELoss = 0.;
+      fTime = gMC->TrackTime() * 1.0e09;
+      fLength = gMC->TrackLength();
+      gMC->TrackPosition(fPos);
+      gMC->TrackMomentum(fMom);
+   }
+   // Sum energy loss for all steps in the active volume
+   fELoss += gMC->Edep();
 
-  // Create AdvTargetPoint at exit of active volume
-  if (  gMC->IsTrackExiting()    ||
-        gMC->IsTrackStop()       ||
-        gMC->IsTrackDisappeared()   ) {
-        if (fELoss == 0. ) {return kFALSE; }
+   // Create AdvTargetPoint at exit of active volume
+   if (gMC->IsTrackExiting() || gMC->IsTrackStop() || gMC->IsTrackDisappeared()) {
+      if (fELoss == 0.) {
+         return kFALSE;
+      }
 
-    fTrackID  = gMC->GetStack()->GetCurrentTrackNumber();
+      fTrackID = gMC->GetStack()->GetCurrentTrackNumber();
 
-    TParticle* p=gMC->GetStack()->GetCurrentTrack();
-    Int_t pdgCode = p->GetPdgCode();
-    TLorentzVector Pos;
-    gMC->TrackPosition(Pos);
-    TLorentzVector Mom;
-    gMC->TrackMomentum(Mom);
-    Int_t detID = 0;
-    gMC->CurrentVolID(detID);
-    const char *name;
-    name = gMC->CurrentVolName();
-    // Check which volume is actually hit and what detID is given
-    LOG(DEBUG)<< "AdvTargetPoint DetID " << detID << " Hit volume name " << name;
-    fVolumeID = detID;
-    Double_t xmean = (fPos.X()+Pos.X())/2. ;
-	Double_t ymean = (fPos.Y()+Pos.Y())/2. ;
-	Double_t zmean = (fPos.Z()+Pos.Z())/2. ;
-    AddHit(fTrackID, detID,  TVector3(xmean, ymean,  zmean),
-			TVector3(fMom.Px(), fMom.Py(), fMom.Pz()), fTime, fLength,
-			fELoss, pdgCode);
+      TParticle *p = gMC->GetStack()->GetCurrentTrack();
+      Int_t pdgCode = p->GetPdgCode();
+      TLorentzVector Pos;
+      gMC->TrackPosition(Pos);
+      TLorentzVector Mom;
+      gMC->TrackMomentum(Mom);
+      Int_t detID = 0;
+      gMC->CurrentVolID(detID);
+      const char *name;
+      name = gMC->CurrentVolName();
+      // Check which volume is actually hit and what detID is given
+      LOG(DEBUG) << "AdvTargetPoint DetID " << detID << " Hit volume name " << name;
+      fVolumeID = detID;
+      Double_t xmean = (fPos.X() + Pos.X()) / 2.;
+      Double_t ymean = (fPos.Y() + Pos.Y()) / 2.;
+      Double_t zmean = (fPos.Z() + Pos.Z()) / 2.;
+      AddHit(fTrackID, detID, TVector3(xmean, ymean, zmean), TVector3(fMom.Px(), fMom.Py(), fMom.Pz()), fTime, fLength,
+             fELoss, pdgCode);
 
-    // Increment number of det points in TParticle
-		ShipStack* stack = (ShipStack*) gMC->GetStack();
-		stack->AddPoint(kAdvSNDTarget);
-    }
-        return kTRUE;
+      // Increment number of det points in TParticle
+      ShipStack *stack = (ShipStack *)gMC->GetStack();
+      stack->AddPoint(kAdvSNDTarget);
+   }
+   return kTRUE;
 }
 
 void AdvTarget::EndOfEvent()
 {
-    fAdvTargetPointCollection->Clear();
+   fAdvTargetPointCollection->Clear();
 }
 
 void AdvTarget::Register()
 {
-    /** This will create a branch in the output tree called
-     AdvTargetPoint, setting the last parameter to kFALSE means:
-     this collection will not be written to the file, it will exist
-     only during the simulation.
-     */
+   /** This will create a branch in the output tree called
+       AdvTargetPoint, setting the last parameter to kFALSE means:
+       this collection will not be written to the file, it will exist
+       only during the simulation.
+   */
 
-    FairRootManager::Instance()->Register("AdvTargetPoint", "AdvTarget",
-                                            fAdvTargetPointCollection, kTRUE);
+   FairRootManager::Instance()->Register("AdvTargetPoint", "AdvTarget", fAdvTargetPointCollection, kTRUE);
 }
-TClonesArray* AdvTarget::GetCollection(Int_t iColl) const
+TClonesArray *AdvTarget::GetCollection(Int_t iColl) const
 {
-    if (iColl == 0) { return fAdvTargetPointCollection; }
-    else { return NULL; }
+   if (iColl == 0) {
+      return fAdvTargetPointCollection;
+   } else {
+      return NULL;
+   }
 }
 
 void AdvTarget::Reset()
 {
-    fAdvTargetPointCollection->Clear();
+   fAdvTargetPointCollection->Clear();
 }
 
-AdvTargetPoint* AdvTarget::AddHit(Int_t trackID, Int_t detID,
-                           TVector3 pos, TVector3 mom,
-                           Double_t time, Double_t length,
-                           Double_t eLoss, Int_t pdgCode)
+AdvTargetPoint *AdvTarget::AddHit(Int_t trackID, Int_t detID, TVector3 pos, TVector3 mom, Double_t time,
+                                  Double_t length, Double_t eLoss, Int_t pdgCode)
 {
-    TClonesArray& clref = *fAdvTargetPointCollection;
-    Int_t size = clref.GetEntriesFast();
-    return new(clref[size]) AdvTargetPoint(trackID, detID, pos, mom,
-                                       time, length, eLoss, pdgCode);
+   TClonesArray &clref = *fAdvTargetPointCollection;
+   Int_t size = clref.GetEntriesFast();
+   return new (clref[size]) AdvTargetPoint(trackID, detID, pos, mom, time, length, eLoss, pdgCode);
 }
-
-
-ClassImp(AdvTarget)
--- shipLHC/AdvTarget.h
+++ shipLHC/AdvTarget.h
@@ -7,13 +7,12 @@
 #ifndef AdvTarget_H
 #define AdvTarget_H
 
-
-#include "FairModule.h"                 // for FairModule
+#include "FairModule.h" // for FairModule
 #include "FairDetector.h"
 
-#include "Rtypes.h"                     // for ShipMuonShield::Class, Bool_t, etc
+#include "Rtypes.h" // for ShipMuonShield::Class, Bool_t, etc
 
-#include <string>                       // for string
+#include <string> // for string
 
 #include "TVector3.h"
 #include "TLorentzVector.h"
@@ -22,91 +21,85 @@ class AdvTargetPoint;
 class FairVolume;
 class TClonesArray;
 
-class AdvTarget : public FairDetector
-{
-public:
-    AdvTarget(const char* name, Bool_t Active, const char* Title="AdvTarget");
-    AdvTarget();
-    virtual ~AdvTarget();
+class AdvTarget : public FairDetector {
+   public:
+      AdvTarget(const char *name, Bool_t Active, const char *Title = "AdvTarget");
+      AdvTarget();
+      virtual ~AdvTarget();
 
-    /** Create the detector geometry **/
-    void ConstructGeometry();
+      /** Create the detector geometry **/
+      void ConstructGeometry();
 
-    void SetConfPar(TString name, Float_t value){conf_floats[name]=value;}
-    void SetConfPar(TString name, Int_t value){conf_ints[name]=value;}
-    void SetConfPar(TString name, TString value){conf_strings[name]=value;}
-    Float_t  GetConfParF(TString name){return conf_floats[name];}
-    Int_t       GetConfParI(TString name){return conf_ints[name];}
-    TString  GetConfParS(TString name){return conf_strings[name];}
+      void SetConfPar(TString name, Float_t value) { conf_floats[name] = value; }
+      void SetConfPar(TString name, Int_t value) { conf_ints[name] = value; }
+      void SetConfPar(TString name, TString value) { conf_strings[name] = value; }
+      Float_t GetConfParF(TString name) { return conf_floats[name]; }
+      Int_t GetConfParI(TString name) { return conf_ints[name]; }
+      TString GetConfParS(TString name) { return conf_strings[name]; }
 
-    /** Initialization of the detector is done here    */
-    virtual void Initialize();
+      /** Initialization of the detector is done here    */
+      virtual void Initialize();
 
-    /**       this method is called for each step during simulation
-    *       (see FairMCApplication::Stepping())
-    */
-    virtual Bool_t ProcessHits( FairVolume* v=0);
+      /**       this method is called for each step during simulation
+       *       (see FairMCApplication::Stepping())
+       */
+      virtual Bool_t ProcessHits(FairVolume *v = 0);
 
-    /**       Registers the produced collections in FAIRRootManager.    */
-    virtual void   Register();
+      /**       Registers the produced collections in FAIRRootManager.    */
+      virtual void Register();
 
-    /** Gets the produced collections */
-    virtual TClonesArray* GetCollection(Int_t iColl) const ;
+      /** Gets the produced collections */
+      virtual TClonesArray *GetCollection(Int_t iColl) const;
 
-    /**      has to be called after each event to reset the containers      */
-    virtual void   Reset();
+      /**      has to be called after each event to reset the containers      */
+      virtual void Reset();
 
-    /**      This method is an example of how to add your own point
-    *       of type muonPoint to the clones array
-    */
-    AdvTargetPoint* AddHit(Int_t trackID, Int_t detID,
-                    TVector3 pos, TVector3 mom,
-                    Double_t time, Double_t length,
-                    Double_t eLoss, Int_t pdgCode);
-    
-    /** The following methods can be implemented if you need to make
-     *  any optional action in your detector during the transport.
-     */
-    
-    virtual void   CopyClones( TClonesArray* cl1,  TClonesArray* cl2 ,
-                              Int_t offset) {;}
-    virtual void   SetSpecialPhysicsCuts() {;}
-    virtual void   EndOfEvent();
-    virtual void   FinishPrimary() {;}
-    virtual void   FinishRun() {;}
-    virtual void   BeginPrimary() {;}
-    virtual void   PostTrack() {;}
-    virtual void   PreTrack() {;}
-    virtual void   BeginEvent() {;}
+      /**      This method is an example of how to add your own point
+       *       of type muonPoint to the clones array
+       */
+      AdvTargetPoint *AddHit(Int_t trackID, Int_t detID, TVector3 pos, TVector3 mom, Double_t time, Double_t length,
+                             Double_t eLoss, Int_t pdgCode);
 
-    AdvTarget(const AdvTarget&);
-    AdvTarget& operator=(const AdvTarget&);
+      /** The following methods can be implemented if you need to make
+       *  any optional action in your detector during the transport.
+       */
 
-private:
-    
-    /** Track information to be stored until the track leaves the
-     active volume.
-     */
-    Int_t          fTrackID;           //!  track index
-    Int_t          fVolumeID;          //!  volume id
-    TLorentzVector fPos;               //!  position at entrance
-    TLorentzVector fMom;               //!  momentum at entrance
-    Double32_t     fTime;              //!  time
-    Double32_t     fLength;            //!  length
-    Double32_t     fELoss;             //!  energy loss
-    /** container for data points */
-    TClonesArray*  fAdvTargetPointCollection;
-    /** configuration parameters **/
-    std::map<TString,Float_t> conf_floats;
-    std::map<TString,Int_t> conf_ints;
-    std::map<TString,TString> conf_strings;
+      virtual void CopyClones(TClonesArray *cl1, TClonesArray *cl2, Int_t offset) { ; }
+      virtual void SetSpecialPhysicsCuts() { ; }
+      virtual void EndOfEvent();
+      virtual void FinishPrimary() { ; }
+      virtual void FinishRun() { ; }
+      virtual void BeginPrimary() { ; }
+      virtual void PostTrack() { ; }
+      virtual void PreTrack() { ; }
+      virtual void BeginEvent() { ; }
 
-    ClassDef(AdvTarget,1)
+      AdvTarget(const AdvTarget &);
+      AdvTarget &operator=(const AdvTarget &);
 
-protected:
-    
-    Int_t InitMedium(const char* name);
-    
+   private:
+      /** Track information to be stored until the track leaves the
+          active volume.
+      */
+      Int_t fTrackID;      //!  track index
+      Int_t fVolumeID;     //!  volume id
+      TLorentzVector fPos; //!  position at entrance
+      TLorentzVector fMom; //!  momentum at entrance
+      Double32_t fTime;    //!  time
+      Double32_t fLength;  //!  length
+      Double32_t fELoss;   //!  energy loss
+      /** container for data points */
+      TClonesArray *fAdvTargetPointCollection;
+      /** configuration parameters **/
+      std::map<TString, Float_t> conf_floats;
+      std::map<TString, Int_t> conf_ints;
+      std::map<TString, TString> conf_strings;
+
+      ClassDef(AdvTarget, 1)
+
+      protected :
+
+      Int_t InitMedium(const char *name);
 };
 
 #endif
