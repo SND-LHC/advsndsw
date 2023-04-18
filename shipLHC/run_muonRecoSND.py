4270afc814e9ae2d30defa6e8fc909bae4904188
siilieva <simona.ilieva.ilieva@cern.ch>
2023-04-18 16:31:16 +0200

    Update setting of the geo file for the HT tracking manager. Added protection against geofile <-> input data mismatch.

--- shipLHC/run_muonRecoSND.py
+++ shipLHC/run_muonRecoSND.py
@@ -6,7 +6,7 @@ import SndlhcMuonReco
 
 parser = ArgumentParser()
 parser.add_argument("-f", "--inputFile", dest="inputFile", help="single input file", required=True)
-parser.add_argument("-g", "--geoFile", dest="geoFile", help="geofile", required=True)
+parser.add_argument("-g", "--geoFile", dest="geoFile", help="geofile", required=False)
 parser.add_argument("-o", "--withOutput", dest="withOutput", help="persistent output", action='store_true',default=False)
 parser.add_argument("-s", "--saveTo", dest="outPath", help="output storage path", type=str,default="",required=False)
 parser.add_argument("-par", "--parFile", dest="parFile", help="parameter file", required=False, default=os.environ['SNDSW_ROOT']+"/python/TrackingParams.xml")
@@ -18,19 +18,50 @@ parser.add_argument("-sc", "--scale",dest="scaleFactor",  help="Run reconstructi
 
 options = parser.parse_args()
 
-import SndlhcGeo
-geo = SndlhcGeo.GeoInterface(options.geoFile)
-
-lsOfGlobals = ROOT.gROOT.GetListOfGlobals()
-lsOfGlobals.Add(geo.modules['Scifi'])
-lsOfGlobals.Add(geo.modules['MuFilter'])
-
 x = options.inputFile
 filename = x[x.rfind('/')+1:]
 if x.rfind('/run_')>0:
-   runN = x[x.rfind('/run_')+4:x.rfind('/run_')+11]
-else: runN = ""
-outFileName = options.outPath+filename.replace('.root',runN+'_muonReco.root')
+   runN = x[x.rfind('/run_')+5:x.rfind('/run_')+11]
+   path =  x[:x.rfind('/run_')+1]
+else:
+     runN = ""
+     path =  x[:x.rfind('/')+1]
+outFileName = options.outPath+filename.replace('.root','_'+runN+'_muonReco.root')
+
+# Set the geometry file
+import SndlhcGeo
+
+if not options.geoFile:
+   if runN == "" : 
+      print('\033[91m'+'Error!'+'\033[0m'+' No run number detected. Must provide a geo file then!')
+      exit()
+   if options.inputFile.find('TI18')<0:
+      if options.inputFile.find('physics/2022')>=0: options.geoFile =  path+"geofile_sndlhc_TI18_V0_2022.root"
+   else:
+     if int(runN) < 4575:
+          options.geoFile = "geofile_sndlhc_TI18_V3_08August2022.root"
+     elif int(runN) < 4855:
+          options.geoFile = "geofile_sndlhc_TI18_V5_14August2022.root"
+     elif int(runN) < 5172:
+          options.geoFile = "geofile_sndlhc_TI18_V6_08October2022.root"
+     else:
+          options.geoFile = "geofile_sndlhc_TI18_V7_22November2022.root"
+     # Introducing SNDLHCEventHeader in 2022 run 4791, so there will be tag!='' in the detector classes
+     # and one has to use the */physics/2022 geofiles
+     if int(runN) >= 4791 and int(runN) <= 5429: path = "/eos/experiment/sndlhc/convertedData/physics/2022/"
+     options.geoFile = path + options.geoFile
+
+# Check geo file is a match for the data
+if (options.inputFile.find('/TI18')>=0 and options.geoFile.find('V0_2022')>=0 ) or \
+   (options.inputFile.find('physics/2022')>=0 and options.geoFile.find('/TI18')>=0 ) or \
+   (options.inputFile.find('/TI18')>=0 and options.geoFile.find('/TI18')>=0 and int(runN) >= 4791 and int(runN) <= 5429 ):
+     print('\033[91m'+'Error!'+'\033[0m'+' Consider a different geo file for that input file! Exitting..')
+     exit()
+
+geo = SndlhcGeo.GeoInterface(options.geoFile)
+lsOfGlobals = ROOT.gROOT.GetListOfGlobals()
+lsOfGlobals.Add(geo.modules['Scifi'])
+lsOfGlobals.Add(geo.modules['MuFilter'])
 
 fullPath = options.inputFile
 if options.inputFile.find('/eos')==0:
@@ -91,4 +122,3 @@ run.Run(options.firstEvent, options.firstEvent + nEvents)
 w.Stop()
 print('Real Time:', w.RealTime(), ' CPU time: ', w.CpuTime())
 print("Done running")
-
