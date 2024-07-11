8d4e68ea498ad1520400603749915a72de7bcfc3
Oliver Lantwin <oliver.lantwin@cern.ch>
2024-07-11 13:54:26 +0200

    Add detector ID decoding to MC points

--- shipLHC/AdvTargetPoint.h
+++ shipLHC/AdvTargetPoint.h
@@ -1,9 +1,7 @@
-#ifndef ADVTARGETPOINT_H
-#define ADVTARGETPOINT_H 1
-
+#ifndef SHIPLHC_ADVTARGETPOINT_H_
+#define SHIPLHC_ADVTARGETPOINT_H_ 1
 
 #include "FairMCPoint.h"
-
 #include "TObject.h"
 #include "TVector3.h"
 
@@ -11,10 +9,8 @@ class AdvTargetPoint : public FairMCPoint
 {
 
   public:
-
     /** Default constructor **/
     AdvTargetPoint();
-
 
     /** Constructor with arguments
      *@param trackID  Index of MCTrack
@@ -26,9 +22,14 @@ class AdvTargetPoint : public FairMCPoint
      *@param eLoss    Energy deposit [GeV]
      **/
 
-    
-    AdvTargetPoint(Int_t trackID, Int_t detID, TVector3 pos, TVector3 mom,
-		Double_t tof, Double_t length, Double_t eLoss, Int_t pdgCode);
+    AdvTargetPoint(Int_t trackID,
+                   Int_t detID,
+                   TVector3 pos,
+                   TVector3 mom,
+                   Double_t tof,
+                   Double_t length,
+                   Double_t eLoss,
+                   Int_t pdgCode);
 
     /** Destructor **/
     virtual ~AdvTargetPoint();
@@ -36,24 +37,23 @@ class AdvTargetPoint : public FairMCPoint
     /** Output to screen **/
     virtual void Print(const Option_t* opt) const;
 
-   Int_t PdgCode() const { return fPdgCode; }
-   int GetStation() { return floor(fDetectorID >> 15); }
-   int GetPlane() { return int(fDetectorID >> 14) % 2; } // 0 is X-plane, 1 is Y-pane
-   int GetRow() { return int(fDetectorID >> 12) % 4; }
-   int GetColumn() { return int(fDetectorID >> 11) % 2; }
-   int GetSensor() { return int(fDetectorID >> 10) % 2; }
-   int GetStrip() { return int(fDetectorID % 768); }
+    Int_t PdgCode() const { return fPdgCode; }
+    int constexpr GetStation() { return fDetectorID >> 17; }
+    int constexpr GetPlane() { return (fDetectorID >> 16) % 2; }   // 0 is X-plane, 1 is Y-pane
+    int constexpr GetRow() { return (fDetectorID >> 13) % 8; }
+    int constexpr GetColumn() { return (fDetectorID >> 11) % 4; }
+    int constexpr GetSensor() { return (fDetectorID >> 10) % 2; }
+    int constexpr GetStrip() { return (fDetectorID) % 1024; }
+    int constexpr GetModule() { return advsnd::target::columns * GetRow() + 1 + GetColumn(); }
+    bool constexpr isVertical() { return GetPlane() == 0; };
 
   private:
-
-
     Int_t fPdgCode;
-    
+
     /** Copy constructor **/
     AdvTargetPoint(const AdvTargetPoint& point);
     AdvTargetPoint operator=(const AdvTargetPoint& point);
     ClassDef(AdvTargetPoint, 1)
-
 };
 
-#endif
+#endif   // SHIPLHC_ADVTARGETPOINT_H_
