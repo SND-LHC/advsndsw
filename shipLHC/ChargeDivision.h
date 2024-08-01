55e02e21b2c4a57a51ddd8458e5d8658a8cc6f48
Nayana Bangaru <nbangaru@cern.ch>
2024-08-01 15:16:57 +0200

    Initial version of energy fluctuation

--- shipLHC/ChargeDivision.h
+++ shipLHC/ChargeDivision.h
@@ -1,6 +1,8 @@
 #ifndef SHIPLHC_CHARGEDIVISION_H_
 #define SHIPLHC_CHARGEDIVISION_H_
 
+#include "AdvTargetPoint.h"
+
 #include <iostream>
 #include <vector>
 // void sumOfTwoNumbers(int a, int b) { std::cout << a << "yo " << b << std::endl; }
@@ -8,7 +10,8 @@ class ChargeDivision
 {
   public:
     ChargeDivision();
-    void ReadPulseShape(std::string test);
+    void ReadPulseShape(std::string PulseFileName);
+    std::vector<float> Divide(Int_t detID, const std::vector<AdvTargetPoint*>& V);
 
   private:
     std::vector<double> PulseValues;
