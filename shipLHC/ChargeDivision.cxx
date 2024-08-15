afa98eadfd86cb314dbbe91d9cd5d8bb64c7b78f
Nayana Bangaru <nbangaru@cern.ch>
2024-08-15 16:50:19 +0200

    Coverted to local coordinates

--- shipLHC/ChargeDivision.cxx
+++ shipLHC/ChargeDivision.cxx
@@ -4,6 +4,39 @@
 #include "EnergyFluctUnit.h"
 #include "SiG4UniversalFluctuation.h"
 #include "TVector3.h"
+#include "TGeoNavigator.h"
+
+#include "FairGeoBuilder.h"
+#include "FairGeoInterface.h"
+#include "FairGeoLoader.h"
+#include "FairGeoMedia.h"
+#include "FairGeoMedium.h"
+#include "FairGeoNode.h"
+#include "FairGeoTransform.h"
+#include "FairGeoVolume.h"
+#include "FairRootManager.h"
+#include "FairVolume.h"
+#include "ShipDetectorList.h"
+#include "ShipStack.h"
+#include "ShipUnit.h"
+#include "SiSensor.h"
+#include "TGeoArb8.h"
+#include "TGeoBBox.h"
+#include "TGeoCompositeShape.h"
+#include "TGeoGlobalMagField.h"
+#include "TGeoManager.h"
+#include "TGeoMaterial.h"
+#include "TGeoMedium.h"
+#include "TGeoSphere.h"
+#include "TGeoTrd1.h"
+#include "TGeoTrd2.h"
+#include "TGeoTube.h"
+#include "TGeoUniformMagField.h"
+#include "TParticle.h"
+#include "TString.h"   // for TString
+#include "TVector3.h"
+#include "TVirtualMC.h"
+
 
 #include <TDatabasePDG.h>
 #include <algorithm>
@@ -56,6 +89,51 @@ void ChargeDivision::ReadPulseShape(std::string PulseFileName)
     // time response not included!
 }
 
+TVector3 ChargeDivision::getLocal(Int_t detID, TVector3 point)
+{
+    TVector3 local_point; 
+    // Calculate the detector id as per the geofile, where strips are disrespected
+    // int strip = (detID) % 1024;                // actual strip ID
+    int geofile_detID = detID; // the det id number needed to read the geometry
+    int station = geofile_detID >> 17;
+    int plane = (geofile_detID >> 16) % 2;
+    int row = (geofile_detID >> 13) % 8;
+    int column = (geofile_detID >> 11) % 4;
+    int sensor = geofile_detID;
+    int sensor_module = advsnd::target::columns * row + 1 + column;
+
+    TString path = TString::Format("/cave_1/"
+                                   "Detector_0/"
+                                   "volAdvTarget_1/"
+                                   "TrackingStation_%d/"
+                                   "TrackerPlane_%d/"
+                                   "SensorModule_%d/"
+                                   "SensorVolumeTarget_%d",
+                                   station,
+                                   plane,
+                                   sensor_module,
+                                   sensor);
+    TGeoNavigator *nav = gGeoManager->GetCurrentNavigator();
+    if (nav->CheckPath(path)) {
+        nav->cd(path);
+    } else {
+        LOG(FATAL) << path;
+    }
+    // Get the corresponding node, which is a sensor made of strips
+    TGeoNode *W = nav->GetCurrentNode();
+    Double_t x = point.X();
+    Double_t y = point.Y();
+    Double_t z = point.Z();
+    double global_pos[3] = {x, y, z};
+    double local_pos[3];
+    nav->MasterToLocal(global_pos, local_pos);
+
+    local_point = local_pos; 
+    return local_point; 
+}
+
+
+
 TVector3 ChargeDivision::DriftDir(TVector3 EntryPoint, TVector3 ExitPoint, float length)
 {
     TVector3 DriftDirUnit = (EntryPoint - ExitPoint).Unit();
@@ -79,10 +157,9 @@ EnergyFluctUnit ChargeDivision::Divide(Int_t detID, const std::vector<AdvTargetP
         // mass, momentum, energy loss in MeV, segment length in mm
         std::vector<Double_t> fluctEnergy;
         std::vector<TVector3> driftPos;
+        std::vector<TVector3> glob_driftPos;
         Int_t pdgcode = V[i]->PdgCode();
-        std::vector<Double_t> dx;
-        std::vector<Double_t> dy;
-        std::vector<Double_t> dz;
+
 
         if (TDatabasePDG::Instance()->GetParticle(pdgcode)) {
             ParticleMass = (TDatabasePDG::Instance()->GetParticle(V[i]->PdgCode())->Mass()) * 1000;
@@ -90,19 +167,27 @@ EnergyFluctUnit ChargeDivision::Divide(Int_t detID, const std::vector<AdvTargetP
         } else {
             cout << "Couldn't find particle " << pdgcode << endl;
         };
+        // cout << "MODULE : " << V[i] -> GetModule() << endl ;
+        // cout << "PLANE : " << V[i] -> GetPlane() << endl; 
+        // cout << "STATION : " << V[i] -> GetStation() << endl; 
+        // cout << "DETECTOR : " << V[i] -> GetDetectorID() << endl; 
+        
 
         // GET THE LOCAL POSITION
+        TVector3 local_entry_point = getLocal(V[i] -> GetDetectorID(), V[i]->GetEntryPoint());
+        TVector3 local_exit_point = getLocal(V[i] -> GetDetectorID(), V[i]->GetExitPoint());
+
 
         int strip = (detID) % 1024;
 
-        double len = (V[i]->GetEntryPoint() - V[i]->GetExitPoint()).Mag();
+        double len = (local_entry_point - local_exit_point).Mag();
 
         if (fabs(ParticleMass) < 1e-6 || ParticleCharge == 0) {
             NumberofSegments = 1;
         } else {
             NumberofSegments = 1
                                + (ChargeDivisionsperStrip
-                                  * abs((V[i]->GetEntryPoint().X() - V[i]->GetExitPoint().X())
+                                  * abs((local_entry_point.X() - local_exit_point.X())
                                         / StripPitch));   // check if this is the correct way
         }
 
@@ -122,17 +207,13 @@ EnergyFluctUnit ChargeDivision::Divide(Int_t detID, const std::vector<AdvTargetP
                 // sig4fluct.SampleFluctuations(ParticleMass, ParticleCharge, Emean, momentum, segLen);
                 fluctEnergy.push_back(
                     sig4fluct.SampleFluctuations(ParticleMass, ParticleCharge, Emean, momentum, segLen));
-                driftPos.push_back(DriftDir(V[i]->GetEntryPoint(), V[i]->GetExitPoint(), (segLen * j) / 20));
-                dx.push_back(driftPos[j].X());
-                dy.push_back(driftPos[j].Y());
-                dz.push_back(driftPos[j].Z());
+                driftPos.push_back(DriftDir(local_entry_point, local_exit_point, (segLen * j) / 20));
+                glob_driftPos.push_back(DriftDir(V[i]->GetEntryPoint(), V[i]->GetExitPoint(), (segLen * j) / 20));
             }
         } else {
             fluctEnergy.push_back(V[i]->GetEnergyLoss() * 1000);
-            driftPos.push_back(DriftDir(V[i]->GetEntryPoint(), V[i]->GetExitPoint(), (segLen) / 20));
-            dx.push_back(V[i]->GetExitPoint().X());
-            dy.push_back(V[i]->GetExitPoint().Y());
-            dz.push_back(V[i]->GetExitPoint().Z());
+            driftPos.push_back(DriftDir(local_entry_point, local_exit_point, (segLen) / 20));
+            glob_driftPos.push_back(DriftDir(V[i]->GetEntryPoint(), V[i]->GetExitPoint(), (segLen) / 20));
         }
 
         double sume = 0;
@@ -145,7 +226,16 @@ EnergyFluctUnit ChargeDivision::Divide(Int_t detID, const std::vector<AdvTargetP
                 fluctEnergy[m] = (fluctEnergy[m] * rescale_ratio) / 1000;
             }
         }
-        EnergyFluctUnit ELossVector(fluctEnergy, segLen / 10, driftPos);
+        EnergyFluctUnit ELossVector(fluctEnergy, segLen / 10, driftPos, glob_driftPos);
+
+        //         //to check if local position is working 
+
+        // ofstream myfile("local_coordinate.txt", std::ios_base::app);
+        // for (int n = 0; n < size(fluctEnergy); n++) {
+        //     myfile << driftPos[n].X() << "\t" << driftPos[n].Y() << "\t" << driftPos[n].Z() << "\t" << 
+        // } 
+        
+        
         return ELossVector;
     }
 }
