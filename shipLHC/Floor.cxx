d1914d452844f99e9a5b54424999bd2a47d2c277
dcentanni <daniele.centanni@outlook.it>
2025-01-08 17:49:05 +0100

    Changed overall detector position, no Excavation Option. Target center at (x,y)=(-27,57)cm

--- shipLHC/Floor.cxx
+++ shipLHC/Floor.cxx
@@ -193,8 +193,8 @@ void Floor::ConstructGeometry()
          Double_t TargetX = 50.0;
          Double_t CurrentTargetY = 9.564471+5.368296; // current y coordinate of the target low edge, hardcoded due to time constraints, find better way
 
-         Double_t ShiftX = TargetX/2.+0.0033519999999995775+2.;
-         Double_t ShiftY = -CurrentTargetY-20.188581+4+9.078588;
+         Double_t ShiftX = TargetX/2.+0.0033519999999995775-27;
+         Double_t ShiftY = -CurrentTargetY+35.890007+0.5384230000000017;
          Double_t ShiftZ = -200;
          ////////////////////////////////////////////
          auto localSND_physCS_comb = new TGeoCombiTrans("localSND_physCS",0.+ShiftX,0.+ShiftY, 0.+ShiftZ,localSND_physCS_rot);    // origin is 480m downstream of IP1, shifting the apparatus 4m upstream
