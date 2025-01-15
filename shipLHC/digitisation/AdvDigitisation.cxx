51ad90cefb9108a9bcf11728292a426b16230d34
Nayana Bangaru <nbangaru@cern.ch>
2025-01-15 09:47:17 +0100

    Added noise classes

--- shipLHC/digitisation/AdvDigitisation.cxx
+++ shipLHC/digitisation/AdvDigitisation.cxx
@@ -21,62 +21,23 @@
 #include <fstream>
 using namespace std;
 
+// Running the digitisation 
+
 AdvDigitisation::AdvDigitisation() {}
 
 std::vector<AdvSignal> AdvDigitisation::digirunoutput(Int_t detID, const std::vector<AdvTargetPoint *> &V)
 {
+    // Charge Division
     ChargeDivision chargedivision{};
     std::vector<EnergyFluctUnit> EnergyLossVector = chargedivision.Divide(detID, V);
 
+    //Charge Drift
     ChargeDrift chargedrift{};
     std::vector<SurfaceSignal> DiffusionSignal = chargedrift.Drift(EnergyLossVector);
 
+    //Induced Charge on strips
     InducedCharge inducedcharge{};
     std::vector<AdvSignal> ResponseSignal = inducedcharge.IntegrateCharge(DiffusionSignal);
 
     return ResponseSignal;
-}
-
-
-TVector3 AdvDigitisation::getLocal(Int_t detID, TVector3 point)
-{
-    TVector3 local_point; 
-    // Calculate the detector id as per the geofile, where strips are disrespected
-    // int strip = (detID) % 1024;                // actual strip ID
-    int geofile_detID = detID; // the det id number needed to read the geometry
-    int station = geofile_detID >> 17;
-    int plane = (geofile_detID >> 16) % 2;
-    int row = (geofile_detID >> 13) % 8;
-    int column = (geofile_detID >> 11) % 4;
-    int sensor = geofile_detID;
-    int sensor_module = advsnd::target::columns * row + 1 + column;
-
-    TString path = TString::Format("/cave_1/"
-                                   "Detector_0/"
-                                   "volAdvTarget_1/"
-                                   "TrackingStation_%d/"
-                                   "TrackerPlane_%d/"
-                                   "SensorModule_%d/"
-                                   "SensorVolumeTarget_%d",
-                                   station,
-                                   plane,
-                                   sensor_module,
-                                   sensor);
-    TGeoNavigator *nav = gGeoManager->GetCurrentNavigator();
-    if (nav->CheckPath(path)) {
-        nav->cd(path);
-    } else {
-        LOG(FATAL) << path;
-    }
-    // Get the corresponding node, which is a sensor made of strips
-    TGeoNode *W = nav->GetCurrentNode();
-    Double_t x = point.X();
-    Double_t y = point.Y();
-    Double_t z = point.Z();
-    double global_pos[3] = {x, y, z};
-    double local_pos[3];
-    nav->MasterToLocal(global_pos, local_pos);
-
-    local_point = local_pos; 
-    return local_point; 
 }
