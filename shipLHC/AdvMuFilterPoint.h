55584d7cf9996e5bcd22235d17e5ca37bfa9986a
fmei <fmei@cern.ch>
2025-11-24 14:38:50 +0100

    adding const where needed

--- shipLHC/AdvMuFilterPoint.h
+++ shipLHC/AdvMuFilterPoint.h
@@ -39,21 +39,18 @@ class AdvMuFilterPoint : public FairMCPoint
     virtual void Print(const Option_t* opt) const;
 
     Int_t PdgCode() const { return fPdgCode; }
-    int constexpr GetLayer() { return fDetectorID >> 17; }
-    int constexpr GetPlane() { return (fDetectorID >> 16) % 2; }   // 0 is X-plane, 1 is Y-pane
-    int constexpr GetRow() { return (fDetectorID >> 13) % 8; }
-    int constexpr GetColumn() { return (fDetectorID >> 11) % 4; }
-    int constexpr GetSensor() { return (fDetectorID >> 10) % 2; }
-    int constexpr GetStrip() { return (fDetectorID) % 1024; }
-    int constexpr GetModule() { return advsnd::hcal::columns * GetRow() + 1 + GetColumn(); }
-    bool constexpr isVertical() { return GetPlane() == 1; };
+    int constexpr GetLayer() const { return fDetectorID >> 17; }
+    int constexpr GetPlane() const { return (fDetectorID >> 16) % 2; }   // 0 is X-plane, 1 is Y-pane
+    int constexpr GetRow() const { return (fDetectorID >> 13) % 8; }
+    int constexpr GetColumn() const { return (fDetectorID >> 11) % 4; }
+    int constexpr GetSensor() const { return (fDetectorID >> 10) % 2; }
+    int constexpr GetStrip() const { return (fDetectorID) % 1024; }
+    int constexpr GetModule() const { return advsnd::hcal::columns * GetRow() + 1 + GetColumn(); }
+    bool constexpr isVertical() const { return GetPlane() == 1; };
 
   private:
     Int_t fPdgCode;
 
-    /** Copy constructor **/
-    AdvMuFilterPoint(const AdvMuFilterPoint& point);
-    AdvMuFilterPoint operator=(const AdvMuFilterPoint& point);
     ClassDef(AdvMuFilterPoint, 1)
 };
 
