5a1bf674ef3cc301687c96bdabc1c98c1c39cc7a
fmei <fmei@cern.ch>
2026-02-16 13:52:46 +0100

    update

--- digitization/digitize_mc.cxx
+++ digitization/digitize_mc.cxx
@@ -29,10 +29,14 @@ TGeoNavigator* initGeometry(const std::string& geometry_path)
 }
 
 int main() {
-    ROOT::RDF::RSnapshotOptions opts;
-    opts.fOutputFormat = ROOT::RDF::ESnapshotOutputFormat::kRNTuple;
+    bool use_rntuple{true};
 
-    auto df = ROOT::RDataFrame("cbmsim", "/afs/cern.ch/work/f/fmei/private/RNTuples-for-advsndsw/dataset/rntuples-sndLHC.Ntuple-TGeant4.root");
+    ROOT::RDF::RSnapshotOptions opts;
+    if (use_rntuple) opts.fOutputFormat = ROOT::RDF::ESnapshotOutputFormat::kRNTuple;
+
+    std::string input_file = use_rntuple ? "/afs/cern.ch/work/f/fmei/private/RNTuples-for-advsndsw/dataset/rntuples-sndLHC.Ntuple-TGeant4.root" : "/afs/cern.ch/work/f/fmei/private/RNTuples-for-advsndsw/dataset/sndLHC.Ntuple-TGeant4-surgery.root";
+    std::string output_file = use_rntuple ? "/afs/cern.ch/work/f/fmei/private/RNTuples-for-advsndsw/dataset/rntuples-df-compiled-sndLHC.Ntuple-TGeant4_digCPP.root" : "/afs/cern.ch/work/f/fmei/private/RNTuples-for-advsndsw/dataset/ttree-df-compiled-sndLHC.Ntuple-TGeant4_digCPP.root";
+    auto df = ROOT::RDataFrame("cbmsim", input_file);
 
     std::string geometry_path = "/afs/cern.ch/work/f/fmei/private/RNTuples-for-advsndsw/dataset/geofile_full.Ntuple-TGeant4.root";
     TGeoNavigator* nav = initGeometry(geometry_path);
@@ -41,5 +45,5 @@ int main() {
     auto df3 = df2.Define("Digi_AdvTargetHits2MCPoints", advsnd::LinkPointsToDigi<AdvTargetPoint>(nav), {"AdvTargetPoint"});
     auto df4 = df3.Define("Digi_AdvMuFilterHits", advsnd::DigitizePoints<AdvMuFilterPoint, AdvMuFilterHit>(nav), {"AdvMuFilterPoint"});
     auto df5 = df4.Define("Digi_AdvMuFilterHits2MCPoints", advsnd::LinkPointsToDigi<AdvMuFilterPoint>(nav), {"AdvMuFilterPoint"});
-    df5.Snapshot("cbmsim", "/afs/cern.ch/work/f/fmei/private/RNTuples-for-advsndsw/dataset/rntuples-df-compiled-sndLHC.Ntuple-TGeant4_digCPP.root", {"MCTrack", "AdvMuFilterPoint", "AdvTargetPoint", "Digi_AdvTargetHits", "Digi_AdvTargetHits2MCPoints", "Digi_AdvMuFilterHits","Digi_AdvMuFilterHits2MCPoints"}, opts);
+    df5.Snapshot("cbmsim", output_file, {"MCTrack", "AdvMuFilterPoint", "AdvTargetPoint", "Digi_AdvTargetHits", "Digi_AdvTargetHits2MCPoints", "Digi_AdvMuFilterHits","Digi_AdvMuFilterHits2MCPoints"}, opts);
 }
